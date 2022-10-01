045847b40ba80cd61473836c613a55b4bd8a987f
Pedro Lourenco <75386625+pedro123456789-wq@users.noreply.github.com>
2022-10-01 18:21:23 +0100

    Update GeneticAlgrithmOptimization.java

--- GeneticAlgrithmOptimization.java
+++ GeneticAlgrithmOptimization.java
@@ -1,13 +1,16 @@
 import java.util.LinkedList;
 import java.util.Random;
 
+//use of genetic algorithm to find global minima of scalar valued, 2 dimensional function
+//the mutation algorithm simply generates a new instance with random values for the points
+//the breeding algorithm creates a new instance at the midpoint of the two instances with the best fitness functions
+
 public class GeneticAlgorithm {
 	private final int maxIterations;
 	private final int initialSize;
 	private final Random randomizer;
 	private LinkedList<double[]> instances;
 	int mutationChance;
-	
 
 	public GeneticAlgorithm(int maxIterations, int initialSize, int mutationChance) {
 		this.maxIterations = maxIterations;
@@ -18,15 +21,14 @@ public class GeneticAlgorithm {
 
 		this.createInitialPopulation();
 		this.sortInstances();
+		this.showInstances();
 	}
 
-	
 	// function to be minimised
 	public double F(double x, double y) {
 		return (Math.pow(x, 2) + (x * y) + Math.pow(y, 2) + y);
 	}
 
-	
 	private void createInitialPopulation() {
 		// create initial population
 		for (int i = 0; i < initialSize; i++) {
@@ -41,25 +43,23 @@ public class GeneticAlgorithm {
 			this.instances.add(instance);
 		}
 	}
-	
-	
+
 	private double fitnessFunction(double[] instance) {
 		return this.F(instance[0], instance[1]);
 	}
 
-	
 	private void sortInstances() {
-		//use bubble sort to sort instances in descending order
+		// use bubble sort to sort instances in descending order
 		boolean isSorted = false;
-		
+
 		while (!isSorted) {
 			isSorted = true;
-			
-			for (int i = 0; i < this.instances.size() - 1; i ++) {
+
+			for (int i = 0; i < this.instances.size() - 1; i++) {
 				double[] currentInstance = this.instances.get(i);
 				double[] nextInstance = this.instances.get(i + 1);
-				
-				if (this.fitnessFunction(currentInstance) < this.fitnessFunction(nextInstance)) {
+
+				if (this.fitnessFunction(currentInstance) > this.fitnessFunction(nextInstance)) {
 					isSorted = false;
 					this.instances.set(i, nextInstance);
 					this.instances.set(i + 1, currentInstance);
@@ -67,59 +67,76 @@ public class GeneticAlgorithm {
 			}
 		}
 	}
-	
-	
+
+	private void showInstances() {
+		System.out.println("Instances: [");
+
+		for (int i = 0; i < this.instances.size(); i++) {
+			double[] instance = this.instances.get(i);
+
+			System.out.println("Instance " + i);
+			System.out.println("Fitness: " + this.fitnessFunction(instance));
+			System.out.println(instance[0] + ", " + instance[1]);
+			System.out.print("\n");
+		}
+
+		System.out.println("]");
+	}
+
 	private double[] cross(double[] instance1, double[] instance2) {
 		double[] child = new double[instance1.length];
-		//average position to breed instances
-		
-		for (int i = 0; i < instance1.length; i ++) {
+		// average position to breed instances
+
+		for (int i = 0; i < instance1.length; i++) {
 			child[i] = (instance1[i] + instance2[i]) / 2;
 		}
-		
+
 		return child;
 	}
-	
-	
+
 	private void mutate() {
-		//add random mutations to each instance to create new instances
-		//this will ensure that the solution space is covered fully
+		// add random mutations to each instance to create new instances
+		// this will ensure that the solution space is covered fully
 		int randomNumber = this.randomizer.nextInt(this.mutationChance);
-		
-		if (randomNumber == 1) {
-			double[] bestCandidate = this.instances.get(0);
-			double[] child = {bestCandidate[0] + 100, bestCandidate[1] + 100};
-			this.instances.add(child);
+
+		if (randomNumber == 0) {
+			double[] instance = new double[2];
+			for (int x = 0; x < 2; x++) {
+				instance[x] = this.randomizer.nextFloat() * 100 * Math.pow(-1, this.randomizer.nextInt());
+			}
+
+			this.instances.add(instance);
 		}
 	}
-	
-	
 
-	
 	public void optimize() {
-		//Use top 30% of population for breeding
+		// Use top 30% of population for breeding
 		for (int iteration = 0; iteration < this.maxIterations; iteration++) {
 			double bestFitness = this.fitnessFunction(this.instances.get(0));
 			System.out.println("Current Fitness: " + bestFitness);
-			System.out.println(this.instances);
-			
-			System.out.println("Breeding ...");
+
 			double[] candidate1 = this.instances.get(0);
 			double[] candidate2 = this.instances.get(1);
 			this.instances.add(this.cross(candidate1, candidate2));
-			
-			
-			System.out.println("Mutating ...");
+
 			this.mutate();
-			
-			System.out.println("Sorting ...");
-			this.sortInstances(); //re-sort instances after cross over and mutation 
+			this.sortInstances(); // re-sort instances after cross over and mutation
+
+			// remove instance with worst fitness at the end of each iteration
+			for (int i = 0; i < 1; i++) {
+				this.instances.remove(this.instances.size() - 1);
+			}
 		}
+
+		double[] bestInstance = this.instances.get(0);
+		System.out.println("\nAlgorithm finished");
+		System.out.println("Iterations: " + this.maxIterations);
+		System.out.println("Minimum Value: " + this.fitnessFunction(bestInstance));
+		System.out.println("Lowest Point: " + bestInstance[0] + ", " + bestInstance[1]);
 	}
-	
-	
+
 	public static void main(String[] args) {
-		GeneticAlgorithm GA = new GeneticAlgorithm(100, 5, 100);
+		GeneticAlgorithm GA = new GeneticAlgorithm(5000, 7, 10);
 		GA.optimize();
 	}
 }
